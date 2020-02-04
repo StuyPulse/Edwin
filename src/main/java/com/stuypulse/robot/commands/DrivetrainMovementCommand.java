dfa407257075782aebf6cabd8dc6897b911824a4
Sam Belliveau <sam.belliveau@gmail.com>
2020-02-04 18:38:55 -0500

    fixed one last thing with DrivetrainMovementCommand so group can work on it

--- src/main/java/com/stuypulse/robot/commands/DrivetrainMovementCommand.java
+++ src/main/java/com/stuypulse/robot/commands/DrivetrainMovementCommand.java
@@ -7,6 +7,10 @@ import com.stuypulse.robot.subsystems.Drivetrain;
  * align the robot very specifically. It uses the robots encoders and navx to
  * get these values, and will do other things like, drive in a straight line
  * while making sure the angle is correct, and turning before driving.
+ * 
+ * WARNING: It is HIGHLY recommended NOT to combine angle and distance commands
+ * as turning the robot can lead to bad measurements with distance and
+ * visa-versa. It is only included to make code more specific
  */
 public class DrivetrainMovementCommand extends DrivetrainPIDAlignmentCommand {
 
@@ -21,12 +25,18 @@ public class DrivetrainMovementCommand extends DrivetrainPIDAlignmentCommand {
 
         private double mGoalAngle;
         private double mGoalDistance;
+        private boolean mJustTurning;
 
-        public DrivetrainMovementAligner(Drivetrain drivetrain, double angle, double distance) {
+        public DrivetrainMovementAligner(Drivetrain drivetrain, double angle, double distance, boolean justTurning) {
             mDrivetrain = drivetrain;
+            mJustTurning = justTurning;
         }
 
         public double getSpeedError() {
+            if (mJustTurning) {
+                return 0.0;
+            }
+
             // TODO: MEASURE CURRENT DISTANCE DRIVEN AND SUBRACT IT FROM GOAL
             return 0.0;
         }
@@ -41,10 +51,20 @@ public class DrivetrainMovementCommand extends DrivetrainPIDAlignmentCommand {
      * Creates command that moves drivetrain very specific amounts
      * 
      * @param drivetrain the drivetrain you want to move
-     * @param angle      the angle you want it to turn before moving
+     * @param angle      the angle you want it to turn before moving (DO NOT USE)
      * @param distance   the distance you want it to travel
      */
     public DrivetrainMovementCommand(Drivetrain drivetrain, double angle, double distance) {
-        super(drivetrain, new DrivetrainMovementAligner(drivetrain, angle, distance));
+        super(drivetrain, new DrivetrainMovementAligner(drivetrain, angle, distance, false));
+    }
+
+    /**
+     * Creates command that moves drivetrain very specific amounts
+     * 
+     * @param drivetrain the drivetrain you want to move
+     * @param angle      the angle you want it to turn
+     */
+    public DrivetrainMovementCommand(Drivetrain drivetrain, double angle) {
+        super(drivetrain, new DrivetrainMovementAligner(drivetrain, angle, 0.0, true));
     }
 }

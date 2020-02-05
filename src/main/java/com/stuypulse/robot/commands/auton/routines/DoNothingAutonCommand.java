3b33b59710b1aa7e5eb1db53ed5ada30ad9f8b62
VinoLoser <vinowang921@gmail.com>
2020-02-05 17:26:31 -0500

    Create DoNothing Auton

---
+++ src/main/java/com/stuypulse/robot/commands/auton/routines/DoNothingAutonCommand.java
@@ -0,0 +1,12 @@
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandGroupBase;
+
+public class DoNothingAutonCommand extends CommandGroupBase {
+
+    @Override
+    public void addCommands(Command... commands) {
+        // TODO Auto-generated method stub
+    }
+    
+    
+}

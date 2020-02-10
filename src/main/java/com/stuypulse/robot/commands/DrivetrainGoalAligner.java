92e6de4b261fb4b40277bb561fbce17ebbbb4d6e
Sam Belliveau <sam.belliveau@gmail.com>
2020-02-10 16:57:48 -0500

    All the work from the DriveSystem Group (#18)
    * initial commit
    
    * formatted code
    
    * added comments to better explain the setup of the commands
    
    * simplified DrivetrainMovementCommand for newbies tomorrow
    
    * added commands to robot container
    
    * finished alignment command
    
    * finished Drivetrain subsystem, added PIDCalculator commands
    
    * cleaned up some code
    
    * added Aligner
    
    * removed unused import
    
    * renamed some classes, added NavX to drivetrain
    
    * fixed weird formatting [newbie ready]
    
    * fixed one last thing with DrivetrainMovementCommand so group can work on it
    
    * fixed a typo
    
    * fixed small but important typo
    
    * fixed nullptr exception
    
    * added comment, prepared for newbies to write code
    
    * commented constants
    
    * added renee's request for commands
    
    * removed Limelight LEDs from always being on during alignment
    
    * fin
    
    * Finished movement commands
    
    * removed a bunch of Ms, fixed gears with drivetrain alignment
    
    * added debug commands
    
    * renamed everything to remove m and k
    
    * fixed issue with limelight alignment
    
    * added time outs to alignment
    
    * added time outs to alignment
    
    * added correct driver smooth values
    
    * fixed abstract commands
    
    * fixed version
    
    * removed more ms, updated more constants, added more smart numbers, made auto tuning way better
    
    * added todos to remind about values to find in constant
    
    * fixed minor issues
    
    * removed drivetrain pid alignment command
    
    * added drivetrain encoder
    
    * cleaned up some code
    
    * fixed unused imports
    
    * made instant commands
    
    * fixed gear
    
    * fixed code review things
    
    * fixed motor ports
    
    Co-authored-by: Kevin Cai <k1029384756c@gmail.com>
    Co-authored-by: Ivan Wei <44007607+iwei20@users.noreply.github.com>

---
+++ src/main/java/com/stuypulse/robot/commands/DrivetrainGoalAligner.java
@@ -0,0 +1,37 @@
+package com.stuypulse.robot.commands;
+
+import com.stuypulse.robot.Constants.Alignment;
+import com.stuypulse.stuylib.network.limelight.Limelight;
+
+/**
+ * The drivetrain goal aligner is an aligner that uses the limelight to send
+ * instructions to the drivetrain on how it should move to align with the
+ * target.
+ */
+public class DrivetrainGoalAligner implements DrivetrainAlignmentCommand.Aligner {
+
+    private double distance;
+
+    public DrivetrainGoalAligner(double distance) {
+        this.distance = distance;
+    }
+
+    public void init() {
+        // Turn on LEDs for CV
+        Limelight.setLEDMode(Limelight.LEDMode.FORCE_ON);
+    }
+
+    public double getSpeedError() {
+        double goal_pitch = Limelight.getTargetYAngle() + Alignment.Measurements.Limelight.PITCH;
+        double goal_height = Alignment.Measurements.GOAL_HEIGHT - Alignment.Measurements.Limelight.HEIGHT;
+        double goal_dist = goal_height / Math.tan(Math.toRadians(goal_pitch))
+                - Alignment.Measurements.Limelight.DISTANCE;
+
+        // Return the error from the target distance
+        return 0 - (goal_dist - distance);
+    }
+
+    public double getAngleError() {
+        return 0 - (Limelight.getTargetXAngle() + Alignment.Measurements.Limelight.YAW);
+    }
+}

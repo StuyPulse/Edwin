0f6cc8ae540742bd3580f1e4b2d3397845e96829
Renee <rmui10@stuy.edu>
2020-02-10 16:46:05 -0500

    Fix intake issues related to code review

--- src/main/java/com/stuypulse/robot/commands/IntakeExtendCommand.java
+++ src/main/java/com/stuypulse/robot/commands/IntakeExtendCommand.java
@@ -4,7 +4,6 @@ import com.stuypulse.robot.subsystems.Intake;
 import edu.wpi.first.wpilibj2.command.InstantCommand;;
 
 public class IntakeExtendCommand extends InstantCommand {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final Intake intake;
 
   public IntakeExtendCommand(Intake intake) {

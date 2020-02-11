5b57ece27e6b6271f1d293abd36cf708a1f62f2e
Pratham Rawat <prathamrawat815@gmail.com>
2020-02-11 15:07:10 -0800

    Pr/pneumatics (#30)
    * add control to robot.java
    
    * Enable compressor control conditional

--- src/main/java/com/stuypulse/robot/Robot.java
+++ src/main/java/com/stuypulse/robot/Robot.java
@@ -54,6 +54,11 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     updateDashboard();
+    if(compress) {
+      pneumatics.startCompressing();
+    } else {
+      pneumatics.stopCompressing();
+    }
     // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic

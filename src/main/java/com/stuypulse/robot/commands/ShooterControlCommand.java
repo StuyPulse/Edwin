5df5afc5498093452bc71318e741175d0c88a0be
Pratham Rawat <prawat00@stuy.edu>
2020-02-16 13:34:32 -0500

    Add shooter solenoids to code

--- src/main/java/com/stuypulse/robot/commands/ShooterControlCommand.java
+++ src/main/java/com/stuypulse/robot/commands/ShooterControlCommand.java
@@ -1,5 +1,6 @@
 package com.stuypulse.robot.commands;
 
+import com.stuypulse.robot.Constants;
 import com.stuypulse.robot.subsystems.Shooter;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -16,5 +17,12 @@ public class ShooterControlCommand extends InstantCommand {
     @Override
     public void initialize() {
         shooter.setShooterSpeed(targetVelocity);
+
+        if(targetVelocity == Constants.SHOOT_FROM_TRENCH_RPM || targetVelocity == Constants.SHOOT_FROM_FAR_RPM) {
+            shooter.retractHoodSolenoid();
+        }
+        if(targetVelocity == Constants.SHOOT_FROM_INITATION_LINE_RPM) {
+            shooter.extendHoodSolenoid();
+        }
     }
 }

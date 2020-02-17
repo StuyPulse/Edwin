fec4e1fabcddd698a2df03e3be078a30bece592d
Sam Belliveau <sam.belliveau@gmail.com>
2020-02-17 13:02:27 -0500

    Sb/drive system (#39)
    * drivetrain arc command
    
    * initial commit
    
    * formatted code
    
    * added comments to better explain the setup of the commands
    
    * simplified DrivetrainMovementCommand for newbies tomorrow
    
    * added commands to robot container
    
    * finished alignment command
    
    * finished Drivetrain subsystem, added PIDCalculator commands
    
    * cleaned up some code
    
    * added Aligner
    
    * removed unused import
    
    * renamed some classes, added NavX to drivetrain
    
    * fixed weird formatting [newbie ready]
    
    * fixed one last thing with DrivetrainMovementCommand so group can work on it
    
    * fixed a typo
    
    * fixed small but important typo
    
    * fixed nullptr exception
    
    * added comment, prepared for newbies to write code
    
    * commented constants
    
    * added renee's request for commands
    
    * removed Limelight LEDs from always being on during alignment
    
    * fin
    
    * Finished movement commands
    
    * removed a bunch of Ms, fixed gears with drivetrain alignment
    
    * added debug commands
    
    * renamed everything to remove m and k
    
    * fixed issue with limelight alignment
    
    * added time outs to alignment
    
    * added time outs to alignment
    
    * added correct driver smooth values
    
    * fixed abstract commands
    
    * fixed version
    
    * removed more ms, updated more constants, added more smart numbers, made auto tuning way better
    
    * added todos to remind about values to find in constant
    
    * fixed minor issues
    
    * removed drivetrain pid alignment command
    
    * added drivetrain encoder
    
    * cleaned up some code
    
    * fixed unused imports
    
    * made instant commands
    
    * fixed gear
    
    * fixed code review things
    
    * fixed motor ports
    
    * fixed power and wpigamepad
    
    * fixed build error
    
    * made alignment less jerky
    
    * made alignment use arcade drive instead of curvature
    
    * fixed misc issues
    
    * Update RobotContainer.java
    
    * added turn and drive command
    
    * fixed various issues with alignment
    
    * fixed gear shifting
    
    * fixed constants
    
    * made  encoder yield not smart
    
    * uninverted stuff
    
    * updated constants
    
    * fixed even more constants
    
    * hehehehe
    
    * added toFeet(inches)
    
    * fixed bug
    
    Co-authored-by: Kevin Cai <k1029384756c@gmail.com>
    Co-authored-by: Ivan Wei <44007607+iwei20@users.noreply.github.com>
    Co-authored-by: Pratham Rawat <prathamrawat815@gmail.com>

--- src/main/java/com/stuypulse/robot/commands/ShooterControlCommand.java
+++ src/main/java/com/stuypulse/robot/commands/ShooterControlCommand.java
@@ -15,6 +15,6 @@ public class ShooterControlCommand extends InstantCommand {
 
     @Override
     public void initialize() {
-        shooter.setShooterSpeed(targetVelocity);
+        shooter.setTargetVelocity(targetVelocity);
     }
 }

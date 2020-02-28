3cf5882d1a0eb05c3bb29a9558c4a7fbf91b37fa
Prog694 <prog692@gmail.com>
2020-02-28 09:17:37 -0800

    Remove previous distance

--- src/main/java/com/stuypulse/robot/commands/auton/routines/RedSixBallTwoTrenchOneTrenchAutonCommand.java
+++ src/main/java/com/stuypulse/robot/commands/auton/routines/RedSixBallTwoTrenchOneTrenchAutonCommand.java
@@ -31,7 +31,7 @@ public class RedSixBallTwoTrenchOneTrenchAutonCommand extends SequentialCommandG
     public RedSixBallTwoTrenchOneTrenchAutonCommand(Drivetrain drivetrain, Shooter shooter, Funnel funnel, Chimney chimney, Intake intake, LEDController controller) {
 
         final double DISTANCE_TO_ACQUIRE_TWO_BALLS_IN_FEET = 5 * 1.208;
-        final double DISTANCE_TO_ACQUIRE_THIRD_BALL_IN_FEET = 5 * 1.208 + 0.5 + 2;
+        final double DISTANCE_TO_ACQUIRE_THIRD_BALL_IN_FEET = 5 * 1.208 + 0.5;
         final double ANGLE_TO_ACQUIRE_FROM_TRENCH_IN_DEGREES = 15 * 1.208;
         final double DRIVETRAIN_SPEED_LIMIT = 0.55;
 

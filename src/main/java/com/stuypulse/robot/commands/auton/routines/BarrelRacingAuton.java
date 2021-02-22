20e7bfbd9a509bfd0d324e0af4ab7b54a362de9e
KevinLi0711 <kli30@stuy.edu>
2021-02-22 17:57:39 -0500

    update imports

--- src/main/java/com/stuypulse/robot/commands/auton/routines/BarrelRacingAuton.java
+++ src/main/java/com/stuypulse/robot/commands/auton/routines/BarrelRacingAuton.java
@@ -1,7 +1,9 @@
 package com.stuypulse.robot.commands.auton.routines;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-      
+import com.stuypulse.commands.auton.DrivetrainRamseteCommand;
+import com.stuypulse.robot.subsystems.Drivetrain;
+
 public class BarrelRacingAuton extends SequentialCommandGroup {
 
     private static final String BARREL_PATH = "BarrelRacingPath/output/BarrelRacing.wpilib.json";

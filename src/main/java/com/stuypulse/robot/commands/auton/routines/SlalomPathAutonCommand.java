69bff71b696178973516d8fd403f079255f66897
ShauryaSen <shauryasen12@gmail.com>
2021-02-24 17:13:09 -0500

    Fix error
    Co-authored-by: Anthony Chen <ACPlayGames@users.noreply.github.com>
    Co-authored-by: Eric Lin <Badbunnies2@users.noreply.github.com>
    Co-authored-by: Lazyyy131 <Lazyyy131@users.noreply.github.com>

--- src/main/java/com/stuypulse/robot/commands/auton/routines/SlalomPathAutonCommand.java
+++ src/main/java/com/stuypulse/robot/commands/auton/routines/SlalomPathAutonCommand.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.Filesystem;
 public class SlalomPathAutonCommand extends DrivetrainRamseteCommand {
     public SlalomPathAutonCommand(Drivetrain drivetrain) {
         super(
-            drivetrain, Filesystem.getDeployDirectory() + "SlalomPath/Slalom.wpilib.json"
+            drivetrain, "SlalomPath/Slalom.wpilib.json"
         ); 
 
         robotRelative(); 

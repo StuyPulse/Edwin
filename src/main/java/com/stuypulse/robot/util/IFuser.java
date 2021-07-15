39ad32a717f2e240e6659daaad2aac3fef8731c7
Myles Pasetsky <mp3blue99@gmail.com>
2021-07-14 22:19:28 -0400

    Add stuylib-style documentation for IFuser

--- src/main/java/com/stuypulse/robot/util/IFuser.java
+++ src/main/java/com/stuypulse/robot/util/IFuser.java
@@ -6,11 +6,32 @@ import com.stuypulse.stuylib.streams.filters.IFilter;
 
 import com.stuypulse.stuylib.streams.filters.LowPassFilter;
 
+/**
+ * A "fuser" combins a low frequency input stream and a 
+ * high frequency input stream using a low pass and high pass filter.
+ * 
+ * This fuser is implemented as a stream itself, and takes in two others.
+ * 
+ * TOOD: move to stuylib?
+ * 
+ * @author Myles Pasetsky (@selym3)
+ */
 public class IFuser implements IStream {
 
-    private IFilter lowPass, highPass;
+    // Two input streams: one being low frequency updates and the other 
+    // being high frequency updates
     private IStream lowFreq, highFreq;
 
+    // The low pass and high pass filters
+    private IFilter lowPass, highPass;
+
+    /**
+     * Creates an input fuser stream
+     * 
+     * @param rc window size value given to both the high and low pass filter
+     * @param lowFreq an input stream of low frequency data 
+     * @param highFreq an input stream of high frequency data
+     */
     public IFuser(Number rc, IStream lowFreq, IStream highFreq) {
         lowPass = new LowPassFilter(rc);
         highPass = new HighPassFilter(rc);
@@ -19,19 +40,27 @@ public class IFuser implements IStream {
         this.highFreq = highFreq;
     }
 
-    public IFuser(Number rc, IStream stream) {
-        this(rc, stream, stream);
-    }
-
+    /**
+     * @return data from the low frequency stream through the low pass filter
+     */
     private double getLowPass() {
         return lowPass.get(lowFreq.get());
     }
 
+    /**
+     * @return data from the high frequency stream through the high pass filter
+     */
     private double getHighPass() {
         return highPass.get(highFreq.get());
     }
 
-    @Override
+    /**
+     * Returns the combined low pass and high pass data.
+     * 
+     * Provides the IStream interface for an IFuser.
+     * 
+     * @return the combined low pass and high pass data
+     */
     public double get() {
         return (getLowPass() + getHighPass());
     }

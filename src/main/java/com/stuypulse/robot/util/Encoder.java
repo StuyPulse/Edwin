a191c0a2a84dab3362ae7f9a68dba21fcb615286
Myles Pasetsky <mp3blue99@gmail.com>
2021-07-20 14:31:04 -0400

    Add javadocs to Encoder wrapper

--- src/main/java/com/stuypulse/robot/util/Encoder.java
+++ src/main/java/com/stuypulse/robot/util/Encoder.java
@@ -4,40 +4,71 @@ import com.revrobotics.CANEncoder;
 import com.stuypulse.robot.subsystems.Drivetrain.Gear;
 
 /**
- * TODO: add javadocs !!!
- * TODO: Velocity needs major checking !!!
- * TODO: make this an encoder subsystem  ???
+ * A wrapper class for a 'raw' encoder that provides 
+ * additional functionality like handling gear shifting and
+ * can make the encoder easier to use / the methods easier to 
+ * understand. 
+ * 
+ * TODO: velocity needs to be tested
  * 
  * @author Myles Pasetsky (@selym3)
  */
 public class Encoder {
     
+    // The encoder class that is closest to hardware
     private CANEncoder encoder;
 
+    // These values are used to integrate an accurate distance
+    // while switching gears 
     private double accumulated;
     private double lastRotations;
 
+    /**
+     * Create an Encoder wrapper
+     * 
+     * @param encoder the underlying encoder class
+     */
     public Encoder(CANEncoder encoder) {
         this.encoder = encoder; 
         reset();
     }
 
+    /**
+     * Get the raw encoder rotations (the unscaled value)
+     * 
+     * @return encoder rotations
+     */
     public double getRotations() {
         return encoder.getPosition();
     }
 
+    /**
+     * Gets the distance the encoder has traveled (the scaled value)
+     * 
+     * @return distance traveled
+     */
     public double getDistance() {
         return accumulated;
     }
     
+    /**
+     * Gets the raw rotations per minute (the unscaled value)
+     * 
+     * @return rotations per minute
+     */
     public double getRPM() {
         return encoder.getVelocity();
     }
 
+    // This would return the scaled rotations per minute (real velocity)
     // public double getVelocity() {
     //     return (implement delta accumulated)
     // }
 
+    /**
+     * Resets the state of the encoder -- both the underlying 
+     * encoder and the addtional measurements. 
+     */
     public void reset() {
         // Reset encoder tracking
         encoder.setPositionConversionFactor(1.0); // <-- always ensure this is true
@@ -48,6 +79,11 @@ public class Encoder {
         lastRotations = 0.0;
     }
 
+    /**
+     * Updates the state of the encoder, based on a gear
+     * 
+     * @param gear gear of the encoder
+     */
     public void periodic(Gear gear) {
         final double nowRotations = getRotations();
 
@@ -57,5 +93,7 @@ public class Encoder {
         accumulated += distance;
         lastRotations = nowRotations;
     }
+
+    
 
 }

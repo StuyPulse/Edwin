c8a9859e0c6cf2b78ca25e28992614d46905599d
Myles Pasetsky <mp3blue99@gmail.com>
2021-07-21 01:34:34 -0400

    Implement new color sensor

--- src/main/java/com/stuypulse/robot/subsystems/ColorSensor.java
+++ src/main/java/com/stuypulse/robot/subsystems/ColorSensor.java
@@ -9,12 +9,66 @@ import com.revrobotics.ColorSensorV3;
 
 import com.stuypulse.robot.Constants.Colors;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ColorSensor extends SubsystemBase {
+
+    // Woof Color enum 
+    public static enum WColor {
+        // Create all colors on the control panel
+        YELLOW,
+        CYAN,
+        GREEN,
+        RED,
+        
+        // To avoid null pointer exceptions when returning error states
+        NONE;
+
+        // Each color will store information about its position
+        private WColor next, rotated;
+
+        // Use a static block to setup circular references
+        static {
+            /*
+            Control Panel Reference: 
+            https://firstfrc.blob.core.windows.net/frc2021/Manual/Sections/2021FRCGameManualSection03.pdf
+            */
+
+            // Setup Yellow
+            YELLOW.next = CYAN; 
+            YELLOW.rotated = GREEN;
+            
+            // Setup Cyan
+            CYAN.next = GREEN;
+            CYAN.rotated = RED;
+            
+            // Setup Green
+            GREEN.next = RED;
+            GREEN.rotated = YELLOW;
+
+            // Setup Red
+            RED.next = YELLOW;
+            RED.rotated = CYAN; 
+
+            // Setup Error state
+            NONE.next = NONE;
+            NONE.rotated = NONE;
+        }
+
+        // Getters 
+        public WColor getNextColor() {
+            return this.next;
+        }
+
+        public WColor getRotatedColor() {
+            return this.rotated;
+        }
+
+    };
 
     private ColorSensorV3 colorSensor;
     private ColorMatch colorMatcher;
@@ -23,41 +77,72 @@ public class ColorSensor extends SubsystemBase {
         colorSensor = new ColorSensorV3(Port.kOnboard);
         colorMatcher = new ColorMatch();
 
+        colorMatcher.addColorMatch(Colors.YELLOW_TARGET);
         colorMatcher.addColorMatch(Colors.CYAN_TARGET);
         colorMatcher.addColorMatch(Colors.GREEN_TARGET);
         colorMatcher.addColorMatch(Colors.RED_TARGET);
-        colorMatcher.addColorMatch(Colors.YELLOW_TARGET);
     }
 
+    /**
+     * @return reports the color that should be under the GAME sensor
+     */
+    public WColor getFMSColor() {
+        String gameData = DriverStation.getInstance().getGameSpecificMessage();
+
+        if (gameData == null || gameData.length() <= 0)
+            return WColor.NONE;
+        
+        switch (gameData.charAt(0)) {
+            case 'Y': return WColor.YELLOW;
+            case 'B': return WColor.CYAN;
+            case 'G': return WColor.GREEN;
+            case 'R': return WColor.RED;
+            default: return WColor.NONE;
+        }
+    }
+
+    /**
+     * @return reports the color that should be under OUR sensor (based on the FMS
+     *         color)
+     */
+    public WColor getTargetColor() {
+        return getFMSColor().getRotatedColor();
+    }
+
+    /**
+     * @return gets the color reported by OUR sensor
+     */
     public Color getRawDetectedColor() {
         return colorMatcher.matchClosestColor(colorSensor.getColor()).color;
     }
 
-    public String getDetectedColor() {
-        Color detectedColor = getRawDetectedColor();
+    /**
+     * @return gets the color reported by OUR sensor
+     */
+    public WColor getDetectedColor() {
+        final Color color = getRawDetectedColor();
 
-        if (detectedColor == Colors.CYAN_TARGET) {
-            return "Cyan";
-        } else if (detectedColor == Colors.RED_TARGET) {
-            return "Red";
-        } else if (detectedColor == Colors.GREEN_TARGET) {
-            return "Green";
-        } else if (detectedColor == Colors.YELLOW_TARGET) {
-            return "Yellow";
-        } else {
-            return "Unknown";
-        }
+        if (color == Colors.YELLOW_TARGET) return WColor.YELLOW;
+        if (color == Colors.CYAN_TARGET) return WColor.CYAN;
+        if (color == Colors.GREEN_TARGET) return WColor.GREEN;
+        if (color == Colors.RED_TARGET) return WColor.RED;
+
+        return WColor.NONE;
     }
 
     @Override
     public void periodic() {
         // SmartDashboard
+
         SmartDashboard.putNumber("Color Sensor/Raw Detected Color Red", getRawDetectedColor().red);
         SmartDashboard.putNumber(
                 "Color Sensor/Raw Detected Color Blue", getRawDetectedColor().blue);
         SmartDashboard.putNumber(
                 "Color Sensor/Raw Detected Color Green", getRawDetectedColor().green);
 
-        SmartDashboard.putString("Color Sensor/Detected Color", getDetectedColor());
+        SmartDashboard.putString("Color Sensor/FMS Color", getFMSColor().name());
+        SmartDashboard.putString("Color Sensor/Rotated FMS Color", getTargetColor().name());
+
+        SmartDashboard.putString("Color Sensor/Detected Color", getDetectedColor().name());
     }
 }

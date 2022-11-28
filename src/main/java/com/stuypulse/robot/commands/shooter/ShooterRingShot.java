2c93c94037586519d56be7d146c7d011da345a3a
Vincent Wang <vinowang921@gmail.com>
2022-11-28 16:37:20 -0500

    Add Shooter commands

---
+++ src/main/java/com/stuypulse/robot/commands/shooter/ShooterRingShot.java
@@ -0,0 +1,14 @@
+package com.stuypulse.robot.commands.shooter;
+
+import com.stuypulse.robot.constants.Settings;
+import com.stuypulse.robot.subsystems.Shooter;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class ShooterRingShot extends SequentialCommandGroup {
+    public ShooterRingShot(Shooter shooter) {
+        addCommands(
+                new ShooterExtendHoodCommand(shooter),
+                new ShooterSetRPMCommand(shooter, Settings.Shooter.RING_RPM.get()));
+    }
+}
---
+++ src/main/java/com/stuypulse/robot/commands/shooter/ShooterSetRPMCommand.java
@@ -0,0 +1,21 @@
+package com.stuypulse.robot.commands.shooter;
+
+import com.stuypulse.robot.subsystems.Shooter;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+public class ShooterSetRPMCommand extends InstantCommand {
+    private final Shooter shooter;
+    private final Number targetRPM;
+
+    public ShooterSetRPMCommand(Shooter shooter, Number targetRPM) {
+        this.shooter = shooter;
+        this.targetRPM = targetRPM;
+        addRequirements(shooter);
+    }
+
+    @Override
+    public void initialize() {
+        shooter.setTargetRPM(targetRPM);
+    }
+}

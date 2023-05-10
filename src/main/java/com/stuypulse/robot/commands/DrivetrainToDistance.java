40adfa42eb242f37b3d0529a4d835e858ce33d5d
Keobkeig <angela.xue3@gmail.com>
2023-05-10 17:06:52 -0400

    Add DrivetrainToDistance.java

---
+++ src/main/java/com/stuypulse/robot/commands/DrivetrainToDistance.java
@@ -0,0 +1,51 @@
+package main.java.com.stuypulse.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import com.stuypulse.robot.subsystems.Drivetrain;
+import com.stuypulse.stuylib.control.feedback.PIDController;
+import com.stuypulse.stuylib.network.SmartNumber;
+
+import edu.wpi.first.math.util.Units;
+
+public class DrivetrainToDistance extends CommandBase {
+    
+    private static interface PID {
+        SmartNumber kP = new SmartNumber("DrivetrainToDistance/kP", 0.0);
+        SmartNumber kI = new SmartNumber("DrivetrainToDistance/kI", 0.0);
+        SmartNumber kD = new SmartNumber("DrivetrainToDistance/kD", 0.0);
+    }
+
+    private final Drivetrain drivetrain;
+
+    private final PIDController controller;
+
+
+    private SmartNumber setpoint;
+
+    public DrivetrainToDistance(Drivetrain drivetrain) {
+        this.drivetrain = drivetrain;
+
+        controller = new PIDController(PID.kP, PID.kI, PID.kD);
+
+        setpoint = new SmartNumber("DrivetrainToDistance/Setpoint", 0.0);
+
+        addRequirements(drivetrain);
+    }
+
+    @Override
+    public void execute() {
+        controller.update(setpoint.get(), drivetrain.getDistance());
+
+        drivetrain.tankDriveVolts(controller.getOutput(), controller.getOutput());
+    }
+
+    @Override
+    public boolean isFinished() {
+        return controller.isDone(Units.inchesToMeters(1.0));
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drivetrain.stop();
+    }
+}
